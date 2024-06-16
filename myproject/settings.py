29ecff8780b07180000299b4b5b049800edc9ca4
rei morofuji <rei.mrfj.0503@gmail.com>
2024-06-16 13:45:08 +0900

    polls を追加しました

--- myproject/settings.py
+++ myproject/settings.py
@@ -75,8 +75,17 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': '08marty25',
+        'HOST': 'localhost',
+        # 'HOST': '160.251.197.137',
+        # 'PORT': '3306',  # MySQLのポート番号に合わせて設定
+        'PORT': '',
+        'OPTIONS': {
+            'unix_socket': '/tmp/mysql.sock',
+        },
     }
 }
 
